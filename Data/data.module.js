3d284a47545526aaf1abc78fe488f4daeb70437c
nikkcartwright13 <125912421+nikkcartwright13@users.noreply.github.com>
2024-08-01 12:14:39 +0400

    all

--- Data/data.module.js
+++ Data/data.module.js
@@ -160,12 +160,11 @@ module.exports = [
         "year": 2009,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/047/414/original/Subaru-Forester-2016-blue.png?1689232942',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/Camry1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/camry2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/camry3.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/camry4.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/camry5.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/camry6.jpeg'
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/subaru/subaru.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/subaru/subaru1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/subaru/subaru3.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/subaru/subaru4.jpeg?raw=true',
+           
 
 
         ],
@@ -183,9 +182,9 @@ module.exports = [
         "year": 2010,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/062/503/original/Nissan-Versa-2009-silver.jpg?1717133546',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Nissan/Versa.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Nissan/Versa1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Nissan/Versa3.jpeg',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Nissan/Versa.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Nissan/Versa1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Nissan/Versa3.jpeg?raw=true',
 
 
 
@@ -205,11 +204,11 @@ module.exports = [
         "year": 2008,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/036/076/original/Suzuki-Vitara-2011-black-r.jpg?1671443747',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzuki.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzuki1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzuki3.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzuki4.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzuki5.jpeg',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzuki.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzuki1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzuki3.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzuki4.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzuki5.jpeg?raw=true',
 
 
         ],
@@ -226,14 +225,14 @@ module.exports = [
         "model": "sx 4",
         "transmission": "Automatic",
         "year": 2008,
-        "image": 'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/sipp.png',
+        "image": 'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/sipp.png?raw=true',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu3.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu4.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu5.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/suzuki/suzu6.jpeg',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu6.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu3.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu4.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/suzuki/suzu5.jpeg?raw=true',
 
 
         ],
@@ -252,9 +251,9 @@ module.exports = [
         "year": 2015,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/062/311/original/Toyota-Sienna-2016-black.jpg?1716792333',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/Toyota.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/Toyota1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Toyota/Toyota3.jpeg',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Toyota/Toyota.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Toyota/Toyota1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Toyota/Toyota3.jpeg?raw=true',
 
 
         ],
@@ -272,10 +271,10 @@ module.exports = [
         "year": 2015,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/045/768/original/Hyundai-Tucson-2019-white.jpg?1686297153',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hinday2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/Hyndai/hyndai3.jpeg'
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Hyndai/hinday2.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Hyndai/hyndai.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Hyndai/hyndai2.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/Hyndai/hyndai3.jpeg?raw=true'
 
         ],
     }, {
@@ -292,12 +291,12 @@ module.exports = [
         "year": 2003,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/021/407/original/Mercedes-Benz-SLK-230-2006-silver.jpg?1624897419',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz3.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz5.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/mersedes/mbz6.jpeg',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz2.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz3.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz5.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/mersedes/mbz6.jpeg?raw=true',
 
 
         ],
@@ -316,10 +315,10 @@ module.exports = [
         "year": 2005,
         "image": 'https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/022/021/original/Chrysler-Cruiser-2005-red.jpg?1627467330',
         "gallery": [
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred2.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred1.jpeg',
-            'https://raw.githubusercontent.com/nikkcartwright13/carrent/main/Cars/chrysler/cryslerred4.jpeg'
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/chrysler/cryslerred.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/chrysler/cryslerred1.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/chrysler/cryslerred2.jpeg?raw=true',
+            'https://github.com/nikkcartwright13/cars/blob/main/Cars/chrysler/cryslerred4.jpeg?raw=true'
 
         ],
     },
